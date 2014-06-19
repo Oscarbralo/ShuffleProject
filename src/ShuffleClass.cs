75eafe22b476e5a092b8aa6c1eaf4fdea6ed5262
Oscarbralo <oscarbralo@outlook.com>
2014-06-19 11:26:10 +0200

    Used to shuffle lists using Generics

---
+++ src/ShuffleClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuffleLibrary
+{
+    public class ShuffleClass<T>
+    {
+        public List<T> Shuffle(List<T> objects, int totalElements)
+        {
+            Random random = new Random();
+            List<T> resultList = new List<T>();
+            for (int i = 0; i < totalElements; i++)
+            {
+                int maxElements = objects.Count - 1;
+                int randomElement = random.Next(0, maxElements);
+                resultList.Add(objects[randomElement]);
+                objects.RemoveAt(randomElement);
+            }
+            return resultList;
+        }
+    }
+}
